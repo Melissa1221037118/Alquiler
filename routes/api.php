d5883014d040dfb6eaa4b7f04395ce9a0250b383
Melissa Montaño vallecilla <melissavallecilla0821@gmail.com>
2024-05-17 12:31:58 -0500

    API Routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\api\VehiculoController;
+use App\Http\Controllers\api\ClienteController;
+use App\Http\Controllers\api\ContratoDeAlquilerController;
+use App\Http\Controllers\api\PagoController;
+use App\Http\Controllers\api\ReservaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +22,38 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Rutas para Vehiculos
+Route::post('/vehiculos', [VehiculoController::class, 'store'])->name('vehiculos.store');
+Route::get('/vehiculos', [VehiculoController::class, 'index'])->name('vehiculos.index');
+Route::get('/vehiculos/{vehiculo}', [VehiculoController::class, 'show'])->name('vehiculos.show');
+Route::put('/vehiculos/{vehiculo}', [VehiculoController::class, 'update'])->name('vehiculos.update');
+Route::delete('/vehiculos/{vehiculo}', [VehiculoController::class, 'destroy'])->name('vehiculos.destroy');
+
+// Rutas para Clientes
+Route::post('/clientes', [ClienteController::class, 'store'])->name('clientes.store');
+Route::get('/clientes', [ClienteController::class, 'index'])->name('clientes.index');
+Route::get('/clientes/{cliente}', [ClienteController::class, 'show'])->name('clientes.show');
+Route::put('/clientes/{cliente}', [ClienteController::class, 'update'])->name('clientes.update');
+Route::delete('/clientes/{cliente}', [ClienteController::class, 'destroy'])->name('clientes.destroy');
+
+// Rutas para Contratos de Alquiler
+Route::post('/contratos_de_alquiler', [ContratoDeAlquilerController::class, 'store'])->name('contratos_de_alquiler.store');
+Route::get('/contratos_de_alquiler', [ContratoDeAlquilerController::class, 'index'])->name('contratos_de_alquiler.index');
+Route::get('/contratos_de_alquiler/{contrato}', [ContratoDeAlquilerController::class, 'show'])->name('contratos_de_alquiler.show');
+Route::put('/contratos_de_alquiler/{contrato}', [ContratoDeAlquilerController::class, 'update'])->name('contratos_de_alquiler.update');
+Route::delete('/contratos_de_alquiler/{contrato}', [ContratoDeAlquilerController::class, 'destroy'])->name('contratos_de_alquiler.destroy');
+
+// Rutas para Pagos
+Route::post('/pagos', [PagoController::class, 'store'])->name('pagos.store');
+Route::get('/pagos', [PagoController::class, 'index'])->name('pagos.index');
+Route::get('/pagos/{pago}', [PagoController::class, 'show'])->name('pagos.show');
+Route::put('/pagos/{pago}', [PagoController::class, 'update'])->name('pagos.update');
+Route::delete('/pagos/{pago}', [PagoController::class, 'destroy'])->name('pagos.destroy');
+
+// Rutas para Reservas
+Route::post('/reservas', [ReservaController::class, 'store'])->name('reservas.store');
+Route::get('/reservas', [ReservaController::class, 'index'])->name('reservas.index');
+Route::get('/reservas/{reserva}', [ReservaController::class, 'show'])->name('reservas.show');
+Route::put('/reservas/{reserva}', [ReservaController::class, 'update'])->name('reservas.update');
+Route::delete('/reservas/{reserva}', [ReservaController::class, 'destroy'])->name('reservas.destroy');
